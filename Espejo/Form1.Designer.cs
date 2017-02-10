525dad30432949ef8de615a2086996e19e244d0e
diegoprietto <dprsoftso@gmail.com>
2017-02-10 20:35:41 -0300

    Commit Inicial
    Problemas con Mutex

---
+++ Espejo/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Espejo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txEntrada = new System.Windows.Forms.TextBox();
+            this.btCrearArchivo = new System.Windows.Forms.Button();
+            this.txEspejo = new System.Windows.Forms.TextBox();
+            this.reloj = new System.Windows.Forms.Timer(this.components);
+            this.btEscribir = new System.Windows.Forms.Button();
+            this.chActualizar = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // txEntrada
+            // 
+            this.txEntrada.Location = new System.Drawing.Point(12, 88);
+            this.txEntrada.MaxLength = 100;
+            this.txEntrada.Multiline = true;
+            this.txEntrada.Name = "txEntrada";
+            this.txEntrada.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txEntrada.Size = new System.Drawing.Size(296, 380);
+            this.txEntrada.TabIndex = 0;
+            // 
+            // btCrearArchivo
+            // 
+            this.btCrearArchivo.Location = new System.Drawing.Point(314, 88);
+            this.btCrearArchivo.Name = "btCrearArchivo";
+            this.btCrearArchivo.Size = new System.Drawing.Size(210, 34);
+            this.btCrearArchivo.TabIndex = 1;
+            this.btCrearArchivo.Text = "Crear Archivo";
+            this.btCrearArchivo.UseVisualStyleBackColor = true;
+            this.btCrearArchivo.Click += new System.EventHandler(this.btCrearArchivo_Click);
+            // 
+            // txEspejo
+            // 
+            this.txEspejo.Location = new System.Drawing.Point(530, 88);
+            this.txEspejo.Multiline = true;
+            this.txEspejo.Name = "txEspejo";
+            this.txEspejo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txEspejo.Size = new System.Drawing.Size(296, 380);
+            this.txEspejo.TabIndex = 4;
+            // 
+            // reloj
+            // 
+            this.reloj.Tick += new System.EventHandler(this.reloj_Tick);
+            // 
+            // btEscribir
+            // 
+            this.btEscribir.Location = new System.Drawing.Point(314, 128);
+            this.btEscribir.Name = "btEscribir";
+            this.btEscribir.Size = new System.Drawing.Size(210, 34);
+            this.btEscribir.TabIndex = 2;
+            this.btEscribir.Text = "Escribir en memoria";
+            this.btEscribir.UseVisualStyleBackColor = true;
+            this.btEscribir.Click += new System.EventHandler(this.btEscribir_Click);
+            // 
+            // chActualizar
+            // 
+            this.chActualizar.AutoSize = true;
+            this.chActualizar.Location = new System.Drawing.Point(530, 65);
+            this.chActualizar.Name = "chActualizar";
+            this.chActualizar.Size = new System.Drawing.Size(107, 17);
+            this.chActualizar.TabIndex = 3;
+            this.chActualizar.Text = "Actualizar Espejo";
+            this.chActualizar.UseVisualStyleBackColor = true;
+            this.chActualizar.CheckedChanged += new System.EventHandler(this.chActualizar_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 480);
+            this.Controls.Add(this.chActualizar);
+            this.Controls.Add(this.btEscribir);
+            this.Controls.Add(this.btCrearArchivo);
+            this.Controls.Add(this.txEspejo);
+            this.Controls.Add(this.txEntrada);
+            this.Name = "Form1";
+            this.Text = "Espejo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txEntrada;
+        private System.Windows.Forms.Button btCrearArchivo;
+        private System.Windows.Forms.TextBox txEspejo;
+        private System.Windows.Forms.Timer reloj;
+        private System.Windows.Forms.Button btEscribir;
+        private System.Windows.Forms.CheckBox chActualizar;
+    }
+}
+
---
+++ Espejo/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Espejo
+{
+    public partial class Form1 : Form
+    {
+        //Archivo en memoria
+        MemoryMappedFile memoria;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btCrearArchivo_Click(object sender, EventArgs e)
+        {
+            //Abrir archivo
+            memoria = MemoryMappedFile.CreateNew("testmap", 10000);
+            btCrearArchivo.Enabled = false;
+        }
+
+        private void chActualizar_CheckedChanged(object sender, EventArgs e)
+        {
+            reloj.Enabled = !reloj.Enabled;
+        }
+
+        private void btEscribir_Click(object sender, EventArgs e)
+        {
+            Byte[] buffer = Encoding.ASCII.GetBytes(txEntrada.Text);
+
+            bool mutexCreated;
+            Mutex mutex = new Mutex(true, "testmapmutex", out mutexCreated);
+
+            mutex.WaitOne();
+
+            using (MemoryMappedViewStream stream = memoria.CreateViewStream())
+            {
+                BinaryWriter writer = new BinaryWriter(stream);
+                writer.Write(buffer);
+            }
+            mutex.ReleaseMutex();
+        }
+
+        private void reloj_Tick(object sender, EventArgs e)
+        {
+            bool mutexCreated;
+            Mutex mutex = new Mutex(true, "testmapmutex", out mutexCreated);
+
+            mutex.WaitOne();
+
+            byte[] cadena = new byte[100];
+            using (MemoryMappedViewStream stream = memoria.CreateViewStream())
+            {
+                BinaryReader reader = new BinaryReader(stream);
+
+                //Leer bytes
+                stream.Read(cadena, 0, 100);
+
+                //Escribir en textbox
+                txEspejo.Text = ASCIIEncoding.ASCII.GetString(cadena);
+
+            }
+            mutex.ReleaseMutex();
+        }
+    }
+}
